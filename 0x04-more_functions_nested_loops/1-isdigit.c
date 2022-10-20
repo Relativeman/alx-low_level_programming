1eba3f1a98f3d4eaa5a7b77449b59e9b1c24a616
Relativeman <agochipet@hotmail.com>
2022-10-20 13:55:17 +0000

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,10 +2,13 @@
 
 /**
  * _isdigit - a function that checks for a digit 0-9
- * @c: an integer argument
- * Return: success
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	return (c <= '0' && c >= '9');
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
 }
